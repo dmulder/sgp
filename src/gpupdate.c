73d93e54a0c47017738f10a310275f67ede762d5
David Mulder <dmulder@suse.com>
2018-03-19 10:57:27 -0600

    Use py c-api calls instead of explicit python strings

--- src/gpupdate.c
+++ src/gpupdate.c
@@ -17,7 +17,8 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags,int argc, const
 {
 	int ret;
 	const char *user, *password;
-	char *cmd;
+	PyObject *pName, *pModule, *pFunc;
+	PyObject *pArgs, *pValue;
 
 	ret = pam_get_item(pamh, PAM_USER, &user);
 	if (ret != PAM_SUCCESS) return ret;
@@ -25,13 +26,31 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags,int argc, const
 	ret = pam_get_item(pamh, PAM_AUTHTOK, &password);
 
 	Py_Initialize();
-    asprintf(&cmd, "from gp_exts import user_policy_apply\n"
-                   "user_policy_apply('%s', '%s')\n",
-             user, password
-        );
-	PyRun_SimpleString(cmd);
-	free(cmd);
+	pName = PyUnicode_FromString("gp_exts");
+	pModule = PyImport_Import(pName);
+	Py_DECREF(pName);
 
+	if (pModule != NULL) {
+		pFunc = PyObject_GetAttrString(pModule, "user_policy_apply");
+		if (pFunc && PyCallable_Check(pFunc)) {
+			pArgs = PyTuple_New(2);
+			pValue = PyUnicode_FromString(user);
+			if (!pValue) {
+				return PAM_ABORT;
+			}
+			PyTuple_SetItem(pArgs, 0, pValue);
+			pValue = PyUnicode_FromString(password);
+			if (!pValue) {
+				return PAM_ABORT;
+			}
+			PyTuple_SetItem(pArgs, 1, pValue);
+			PyObject_CallObject(pFunc, pArgs);
+		} else {
+			return PAM_ABORT;
+		}
+	} else {
+		return PAM_ABORT;
+	}
 
 	return PAM_SUCCESS;
 }
