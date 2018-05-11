15d7ac3e40a1757562cef2f675c13db1ac85d57b
David Mulder <dmulder@suse.com>
2018-05-11 16:36:32 -0600

    Strip code being moved to samba

--- src/gp_exts/__init__.py
+++ src/gp_exts/__init__.py
@@ -2,75 +2,38 @@ import logging, os, sys
 from samba.gpclass import apply_gp, GPOStorage
 from samba.param import LoadParm
 from samba.credentials import Credentials
-from samba import NTSTATUSError
-
-# Get a list of modules names
-def list_modules(filename):
-    from os import listdir
-    from os.path import dirname, abspath, splitext
-    module_names = []
-    for f in listdir(dirname(abspath(filename))):
-        split = splitext(f)
-        if not '__init__' in f and (split[-1] == '.py' or split[-1] == '.pyc'):
-            module_names.append(split[0])
-    return list(set(module_names))
-
-# Find the top base class of a class
-# doesn't work with multiple base classes
-def get_base(cls):
-    base = None
-    bases = cls.__bases__
-    while len(bases) > 0 and bases[-1].__name__ != 'object':
-        base = bases[0]
-        bases = base.__bases__
-    return base
-
-def get_gp_exts_from_module(parent):
-    import inspect
-    parent_gp_exts = []
-    for mod_name in parent.modules:
-        mod = getattr(parent, mod_name)
-        clses = inspect.getmembers(mod, inspect.isclass)
-        for cls in clses:
-            base = get_base(cls[-1])
-            if base and base.__name__ == 'gp_ext' and cls[-1].__module__ == mod.__name__:
-                parent_gp_exts.append(cls[-1])
-    return parent_gp_exts
-
-from gp_exts.gpmachine import *
-machine_gp_exts = get_gp_exts_from_module(gpmachine)
-from gp_exts.gpuser import *
-user_gp_exts = get_gp_exts_from_module(gpuser)
-
-import syslog
-class Logger:
-    def warn(self, msg):
-        syslog.syslog(syslog.LOG_ERR, msg)
-
-    fatal = warn
-    critical = warn
-    debug = warn
-    error = warn
-    info = warn
-    log = warn
-    warning = warn
 
 def user_policy_apply(user, password):
     user = user.split('\\')[-1]
-    logger = Logger()
+
+    logger = logging.getLogger('gpupdate')
+    logger.addHandler(logging.StreamHandler(sys.stdout))
+    logger.setLevel(logging.CRITICAL)
+    log_level = lp.log_level()
+    if log_level == 1:
+        logger.setLevel(logging.ERROR)
+    elif log_level == 2:
+        logger.setLevel(logging.WARNING)
+    elif log_level == 3:
+        logger.setLevel(logging.INFO)
+    elif log_level >= 4:
+        logger.setLevel(logging.DEBUG)
     lp = LoadParm()
     lp.load_default()
     creds = Credentials()
     creds.guess(lp)
     creds.set_username(user)
     creds.set_password(password)
+
+    _, user_exts = get_gp_client_side_extensions(logger)
     gp_extensions = []
-    for ext in user_gp_exts:
-        gp_extensions.append(ext(logger, creds))
+    for ext in user_exts:
+        gp_extensions.append(ext(logger, lp, creds, store))
+
     cache_dir = lp.get('cache directory')
     store = GPOStorage(os.path.join(cache_dir, 'gpo.tdb'))
     try:
-        apply_gp(lp, creds, None, logger, store, gp_extensions)
+        apply_gp(lp, creds, logger, store, gp_extensions)
     except NTSTATUSError as e:
         logger.info(e.message)
 
--- src/gp_exts/gpmachine/__init__.py
+++
@@ -1,4 +0,0 @@
-from gp_exts import list_modules
-modules = list_modules(__file__)
-__all__ = modules
-del list_modules
